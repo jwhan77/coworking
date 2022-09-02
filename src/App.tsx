5d8b2e8170408280a58fc5150021de2632665816
jwhan77 <gksjeongw96@gmail.com>
2022-09-02 22:44:41 +0900

    Change naver map api to kakao map api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   useEffect(() => {
-    const mapOptions = {
-      center: new naver.maps.LatLng(37.3595704, 127.105399),
-      zoom: 10
+    var container = document.getElementById('map')!; //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.5330619, 126.6309226), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
     };
-    
-    const map = new naver.maps.Map('map', mapOptions);
-    const size = new naver.maps.Size(600, 400);
+
+    var map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   }, []);
   return (
     <div className="App">
