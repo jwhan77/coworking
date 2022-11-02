de99f76f6b273ce585033a641d3e416268055e55
jwhan77 <gksjeongw96@gmail.com>
2022-11-02 21:12:20 +0900

    Resize map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useWindowSize } from 'usehooks-ts'
 import { Button } from 'react-bootstrap';
 
 import './Map.css'
 
-const Map = ({...props}) => {
+let map: (naver.maps.Map | null) = null;
 
-  let map: (naver.maps.Map | null) = null;
+const Map = ({...props}) => {
+  const { width, height } = useWindowSize()
+  const mapRef = useRef(null);
 
   useEffect(() => {
     if(map === null) {
@@ -13,8 +16,7 @@ const Map = ({...props}) => {
         center: new naver.maps.LatLng(33.4995687, 126.5311287),
         zoom: 15
       });
-      new naver.maps.Size(600, 400);
-      
+      const sizeCompare = new naver.maps.Size(window.innerWidth - 400, window.innerHeight);
       new naver.maps.Marker({
         position: new naver.maps.LatLng(33.4995687, 126.5311287),
         map: map
@@ -32,17 +34,25 @@ const Map = ({...props}) => {
       center: new naver.maps.LatLng(lat, lng),
       zoom: 15
     });
-    new naver.maps.Size(600, 400);
+    new naver.maps.Size(width ? width : window.innerWidth - 400, height ? height : window.innerHeight);
     new naver.maps.Marker({
       position: new naver.maps.LatLng(lat, lng),
       map: map
     })
   }, [props.loc])
 
+  useEffect(() => {
+    if(mapRef !== null) {
+      const mapDiv = mapRef.current as unknown as HTMLDivElement;
+      mapDiv.style.width = `${width}px`
+      mapDiv.style.height = `${height}px`
+    }
+  }, [width, height]);
+
   return (
     <div className='Map'>
-      <Button onClick={props.handleLoad}>Load my location</Button>
-      <div id='map'></div>
+      <Button className="loading" onClick={props.handleLoad}>Load my location</Button>
+      <div id='map' ref={mapRef}></div>
     </div>
   )
 }
